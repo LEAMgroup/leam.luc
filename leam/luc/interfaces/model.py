34498d22375cdd7dd7d417a9b16d7c06b8652f9f
Jeff Terstriep <jefft@leamgroup.com>
2012-08-13 00:55:23 +0000

    Added interface for Model type.  New models will inherit from this base class.

---
+++ leam/luc/interfaces/model.py
@@ -0,0 +1,12 @@
+from zope.interface import Interface
+
+from leam.luc import lucMessageFactory as _
+
+
+class IModel(Interface):
+    """A maker interface for content types that will be integrated into
+    the pop_queue request.
+    """
+
+    pass 
+
