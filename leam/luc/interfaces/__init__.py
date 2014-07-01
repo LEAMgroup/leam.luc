b7f7b9daa7c1d92e93f17407f84a761a098a1539
Jeff Terstriep <jefft@leamgroup.com>
2014-07-01 14:57:58 -0500

    Removed all old archetype files prior to replacing them with dexterity files.

--- leam/luc/interfaces/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- extra stuff goes here -*-
-from projection import IProjection
-from probmap import IProbmap
-from lucscenario import ILUCScenario
-from settings import ILUCSettings
-from model import IModel
